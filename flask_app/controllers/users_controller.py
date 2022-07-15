38fc8a075b4c1983f8216d78138f667d4f9f4799
lukejlackey <lukejlackey@gmail.com>
2022-07-14 17:44:20 -0700

    finished users app

--- flask_app/controllers/users_controller.py
+++ flask_app/controllers/users_controller.py
@@ -6,12 +6,31 @@ from flask import render_template, redirect, request
 def displayUsers():
     return render_template('readAll.html', users= list(User.showAll()))
 
-@app.route('/process', methods=['POST'])
+@app.route('/users/<int:id>')
+def viewUser(id):
+    return render_template('userPage.html', user= User.showUser(id))
+
+@app.route('/users/process', methods=['POST'])
 def addUser():
     new_user = request.form
     User.addNewUser(new_user)
     return redirect('/users')
 
+@app.route('/users/<int:id>/process', methods=['POST'])
+def editUser(id):
+    edits = request.form
+    User.editExistingUser(id, edits)
+    return redirect(f'/users/{id}')
+
 @app.route('/users/new')
 def displayAddUser():
     return render_template('create.html')
+
+@app.route('/users/<int:id>/edit')
+def displayEdit(id):
+    return render_template('editUser.html', user= User.showUser(id))
+
+@app.route('/users/<int:id>/destroy')
+def deleteUser(id):
+    User.deleteExistingUser(id)
+    return redirect('/users')
--- flask_app/models/users_model.py
+++ flask_app/models/users_model.py
@@ -4,20 +4,50 @@ class User:
     
     def __init__(self, data) -> None:
         self.id = data['id']
-        self.full_name = f"{data['first_name']} {data['last_name']}"
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.full_name = f'{self.first_name} {self.last_name}'
         self.email = data['email']
         self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
     
     @classmethod
     def showAll(cls):
-        query = 'SELECT * FROM users'
+        query = 'SELECT * FROM users;'
         result = connectToMySQL('users_schema').query_db(query)
         users = [cls(user).__dict__ for user in result]
+        exclusions = ['first_name','last_name','updated_at']
+        for user in users:
+            for excl in exclusions:
+                del user[excl]
         return users
-    
+
+    @classmethod
+    def showUser(cls, id):
+        query = 'SELECT * FROM users '
+        query += f'WHERE users.id = {id};'
+        result = connectToMySQL('users_schema').query_db(query)
+        result = cls(result.pop())
+        return result
+
     @classmethod
     def addNewUser(cls, data):
-        query = 'INSERT INTO users( first_name, last_name, email )'
-        query += 'VALUES( %(first_name)s, %(last_name)s, %(email)s)'
+        query = 'INSERT INTO users( first_name, last_name, email ) '
+        query += 'VALUES( %(first_name)s, %(last_name)s, %(email)s);'
         result = connectToMySQL('users_schema').query_db(query, data)
+        return result
+
+    @classmethod
+    def editExistingUser(cls, id, data):
+        query = 'UPDATE users '
+        query += 'SET first_name = %(first_name)s, last_name = %(last_name)s, email = %(email)s '
+        query += f'WHERE id = {id};'
+        result = connectToMySQL('users_schema').query_db(query, data)
+        return result
+
+    @classmethod
+    def deleteExistingUser(cls, id):
+        query = 'DELETE FROM users '
+        query += f'WHERE id = {id};'
+        result = connectToMySQL('users_schema').query_db(query)
         return result
